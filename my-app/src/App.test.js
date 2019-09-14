772a01f9b1262fa091ea04291e92801a2cfe663c
Hong Yi Chen <hongyigma@gmail.com>
2019-09-14 15:19:25 -0400

    Created Login.js

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './Login'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import './App.css';
 
-class App extends Component{
+class Login extends Component{
   state = {
     username: null,
     password: null
@@ -37,4 +36,4 @@ class App extends Component{
   }
 }
 
-export default App;
+export default Login;
