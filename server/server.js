37e1804e79e1dabded697eec01780c927709a324
Erica <erica.wang.moyang@gmail.com>
2019-09-15 09:42:25 -0400

    a

--- server/server.js
+++ server/server.js
@@ -9,27 +9,6 @@ const app = express();
 app.use(cors());
 
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    res.setHeader('http://accentour-final-platinum.uedpnpkwfs.us-east-2.elasticbeanstalk.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.post('/signup', function (req, res){
 	axios.post('http://accentour-final-platinum.uedpnpkwfs.us-east-2.elasticbeanstalk.com/create_user?username'
 		+req.body.us2ername).then((res)=>{
@@ -91,7 +70,7 @@ var bookFlights = (url, key, clas, date)=>{
 
 app.post('/loadAirports', function (req, res){
 
-	var departureCity = req.body.departureCity;
+	var departureCity = "California";
 
 	console.log(req.body);
 
