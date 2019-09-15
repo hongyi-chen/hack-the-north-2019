b3901c5d96421bac605489c57f02c594ce0c5942
Tailai Wang <tailaiwang01@gmail.com>
2019-09-15 04:21:00 -0400

    I just wanna nap man

--- my-app/src/brokerSearch.js
+++ my-app/src/brokerSearch.js
@@ -17,53 +17,6 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-class DataController extends React.Component{
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        data: []
-    };
-
-    this.getData = this.getData.bind(this);
-  }
-
-  componentWillMount() {
-      this.getCsvData();
-  }
-
-  fetchCsv() {
-      return fetch('gs://hack-the-north2019.appspot.com/csv/Book1.csv').then(function (response) {
-          let reader = response.body.getReader();
-          let decoder = new TextDecoder('utf-8');
-          console.log(5);
-          return reader.read().then(function (result) {
-              return decoder.decode(result.value);
-          });
-      });
-  }
-
-  getData(result) {
-      this.setState({data: result.data});
-  }
-
-  async getCsvData() {
-      let csvData = await this.fetchCsv();
-
-      Papa.parse(csvData, {
-          complete: this.getData
-      });
-  }
-
-  render() {
-    return (
-        <section className="data-controller">
-            ...
-        </section>
-    );
-  }
-} 
-
 class ProfilePage extends Component {
   state = {
     username: null,
