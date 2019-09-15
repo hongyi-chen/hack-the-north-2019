ee386a44f3af0c4e592653b260a1af6d0e61d6a9
Hong Yi Chen <hongyigma@gmail.com>
2019-09-14 23:42:42 -0400

    25 children

--- my-app/src/brokerSearch.js
+++ my-app/src/brokerSearch.js
@@ -1,79 +1,137 @@
 import React, { Component } from "react";
 import "./tourSearch.css";
+import './assets/css/main.css';
+import "./login.css";
 
 class brokerSearch extends Component {
   render() {
     return (
       <div>
+        <header id="header" className="container">
+          {/* Logo */}
+          <div id="logo">
+            <h1>tourhub</h1>
+            <span>powered by Accenture.</span>
+          </div>
+        </header>
         <div className="search">
           {/* Search Contents */}
           <div className="container fill_height">
-                {/* Search Panel */}
-                <div className="search_panel active">
-                  <form
-                    action="#"
-                    id="search_form_1"
-                    className="search_panel_content d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-lg-between justify-content-start"
-                  >
-                    <div className="search_item">
-                      <div>destination</div>
-                      <input
-                        type="text"
-                        className="destination search_input"
-                        required="required"
-                      />
-                    </div>
-                    <div className="search_item">
-                      <div>check in</div>
-                      <input
-                        type="text"
-                        className="check_in search_input"
-                        placeholder="YYYY-MM-DD"
-                      />
-                    </div>
-                    <div className="search_item">
-                      <div>check out</div>
-                      <input
-                        type="text"
-                        className="check_out search_input"
-                        placeholder="YYYY-MM-DD"
-                      />
-                    </div>
-                    <div className="search_item">
-                      <div>adults</div>
-                      <select
-                        name="adults"
-                        id="adults_1"
-                        className="dropdown_item_select search_input"
-                      >
-                        <option>01</option>
-                        <option>02</option>
-                        <option>03</option>
-                      </select>
-                    </div>
-                    <div className="search_item">
-                      <div>children</div>
-                      <select
-                        name="children"
-                        id="children_1"
-                        className="dropdown_item_select search_input"
-                      >
-                        <option>0</option>
-                        <option>02</option>
-                        <option>03</option>
-                      </select>
-                    </div>
-                    <button className="button search_button">
-                      search
-                      <span />
-                      <span />
-                      <span />
-                    </button>
-                  </form>
+            {/* Search Panel */}
+            <div className="search_panel active">
+              <form
+                action="#"
+                id="search_form_1"
+                className="search_panel_content d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-lg-between justify-content-start"
+              >
+                <div className="search_item">
+                  <div>origin city</div>
+                  <input
+                    type="text"
+                    className="origin search_input"
+                    placeholder="What is the city you are starting from?"
+                    required="required"
+                  />
                 </div>
+                <div className="search_item">
+                  <div>check in</div>
+                  <input
+                    type="text"
+                    className="check_in search_input"
+                    placeholder="YYYY-MM-DD"
+                    required="required"
+                  />
+                </div>
+                <div className="search_item">
+                  <div>check out</div>
+                  <input
+                    type="text"
+                    className="check_out search_input"
+                    placeholder="YYYY-MM-DD"
+                    required="required"
+                  />
+                </div>
+                <div className="search_item">
+                  <div>adults</div>
+                  <select
+                    name="adults"
+                    id="adults_1"
+                    className="dropdown_item_select search_input"
+                    required="required"
+                  >
+                    <option>01</option>
+                    <option>02</option>
+                    <option>03</option>
+                    <option>04</option>
+                    <option>05</option>
+                    <option>06</option>
+                    <option>07</option>
+                    <option>08</option>
+                    <option>09</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    <option>21</option>
+                    <option>22</option>
+                    <option>23</option>
+                    <option>24</option>
+                    <option>25</option>
+                  </select>
+                </div>
+                <div className="search_item">
+                  <div>children</div>
+                  <select
+                    name="children"
+                    id="children_1"
+                    className="dropdown_item_select search_input"
+                    required="required"
+                  >
+                    <option>01</option>
+                    <option>02</option>
+                    <option>03</option>
+                    <option>04</option>
+                    <option>05</option>
+                    <option>06</option>
+                    <option>07</option>
+                    <option>08</option>
+                    <option>09</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    <option>21</option>
+                    <option>22</option>
+                    <option>23</option>
+                    <option>24</option>
+                    <option>25</option>
+                  </select>
+                </div>
+                <button className="button search_button">
+                  search
+                  <span />
+                  <span />
+                  <span />
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-        );  
+        );
       </div>
     );
   }
